4effe7472d7a6e3fe60fe181a646ce2b1218df42
nurzodbek4real <7nurzodbek@gmail.com>
2024-12-07 14:46:29 +0500

    final

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,6 +1,18 @@
-const repeatString = function() {
+const repeatString = function(string, num) {
+  // Handle negative num
+  if (num < 0) {
+    return 'ERROR';
+  }
 
+  let result = "";
+
+  // Repeat the string 'num' times
+  for (let i = 0; i < num; i++) {
+    result += string;
+  }
+
+  return result;
 };
 
-// Do not edit below this line
+// Export the function so it can be used in the test file
 module.exports = repeatString;
